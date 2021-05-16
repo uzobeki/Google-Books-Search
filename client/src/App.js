7d6cf4c48084fb1db974b079bca1b1ee049cf7d6
Zo Beki <uobeki.40@gmail.com>
2021-05-16 19:08:37 -0400

    react development files

---
+++ client/src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Nav from "./components/Nav";
+import Header from "./components/Header";
+import Wrapper from "./components/Wrapper";
+
+import NotFound from "./pages/NotFound";
+import Search from "./pages/Search";
+import Saved from "./pages/Saved";
+
+import "./App.css";
+
+class App extends Component {
+ 
+  render() {
+    return (
+      <Router>
+        <div>
+          <Nav/>
+          <Header />
+          <Wrapper>
+            <Route exact path="/" component={Search} />
+            <Route exact path="/search" component={Search} />
+            <Route exact path="/saved" component={Saved} />
+            <Route exact path="/noMatch" component={NotFound} />
+          </Wrapper>
+        </div>
+      </Router>
+    )
+  };
+};
+
+export default App;
